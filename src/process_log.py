1c25b1c254df53e6e60d1b4eb84fc522842544f0
MitulAP <mitulpatel.hsd@gmail.com>
2017-04-05 17:23:48 -0700

    adding support for file write

--- src/process_log.py
+++ src/process_log.py
@@ -3,8 +3,14 @@
 
 import sys
 import re
+from collections import Counter
 
 input_log_file = sys.argv[1]
+host_output = open(sys.argv[2], 'w')
+hour_output = open(sys.argv[3], 'w')
+resource_output = open(sys.argv[4], 'w')
+block_output = open(sys.argv[5], 'w')
+
 
 DATE_FORMAT = '%d/%b/%Y:%H:%M:%S %z'
 REGEX_FOR_LOG_LINE = '^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] \"(\S+) (\S+)\s*(\S+)?\s*\" (\d{3}) (\S+)'
@@ -26,30 +32,35 @@ def parse_log_line (line):
 			- content_size
 	"""
 
-	match = re.search(REGEX_FOR_LOG_LINE, line)
+	match = re.search(REGEX_FOR_LOG_LINE.encode().decode(), line.encode().decode('utf-8','ignore'))
 
-	host		  = match.group(1)
-	client_identd = match.group(2)
-	user_id       = match.group(3)
-	date_time     = match.group(4)
-	method        = match.group(5)
-	endpoint      = match.group(6)
-	protocol      = match.group(7)
-	response_code = int(match.group(8))
-	content_size  = match.group(9)
+	if match:
+		host		  = match.group(1)
+		client_identd = match.group(2)
+		user_id       = match.group(3)
+		date_time     = match.group(4)
+		method        = match.group(5)
+		endpoint      = match.group(6)
+		protocol      = match.group(7)
+		response_code = int(match.group(8))
+		content_size  = match.group(9)
 
 
-	result_dict = {}
-	result_dict["host"] = host
-	result_dict["date_time"] = date_time
-	result_dict["method"] = method
-	result_dict["url"] = endpoint
-	result_dict["protocol"] = protocol
-	result_dict["response_code"] = response_code
-	result_dict["content_size"] = content_size
+		result_dict = {}
+		result_dict["host"] = host
+		result_dict["date_time"] = date_time
+		result_dict["method"] = method
+		result_dict["url"] = endpoint
+		result_dict["protocol"] = protocol
+		result_dict["response_code"] = response_code
+		result_dict["size"] = content_size
 
-	return result_dict
+		return result_dict
+	else:
+		return None
 	# print("method : ", method, " --- endpoint : ", endpoint, " --- protocol : ", protocol, " --- response_code : ", response_code, " --- content_size : ", content_size)
+
+enc='iso-8859-15'
 
 def read_from_file (file):
 	"""
@@ -59,11 +70,54 @@ def read_from_file (file):
 	"""
 
 	try:
-		with open(file) as f_obj:
+		with open(file, 'r', encoding=enc) as f_obj:
 			for line in f_obj:
-				print(parse_log_line(line))
+				parsed_line = parse_log_line(line)
+				if parsed_line:
+					yield parsed_line
 				
 	except FileNotFoundError:
 		print("File not found")
 
-read_from_file(input_log_file)
+
+def count_host_and_usage_frequency(host_counter, usage_counter,line):
+	"""
+    Counts number of times host/Ip addresses have accessed the website.
+    Arguments:
+        counter (Counter class): stores counts for each host/ip
+        data (dict): parsed log line
+    """
+	if line:
+		host_counter[line["host"]] += 1
+		if line["size"] != "-":
+			usage_counter[line["url"]] += int(line["size"])
+
+host_freq = Counter()
+data_usage_freq = Counter()
+
+print ("Feature 1")
+
+for line in read_from_file(input_log_file):
+	count_host_and_usage_frequency(host_freq, data_usage_freq, line)
+
+hosts_most_freq = host_freq.most_common(10)
+usages_most_freq = data_usage_freq.most_common(10)
+
+for host in hosts_most_freq:
+	host_info = host[0] + "," + str(host[1])
+	host_output.write(host_info + "\n")
+
+for most_freq in usages_most_freq:
+	resource_output.write(most_freq[0] + "\n")
+
+print(hosts_most_freq)
+print(usages_most_freq)
+
+# for host in hosts_most_freq:
+# 	print(host)
+
+# for usage in data_usage_freq:
+# 	print(usage)
+
+print ("Feature 1 - end")
+
